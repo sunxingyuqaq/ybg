bf9aee9ccdffa1450d87af3e3ef49e46d8e9476b
sunxingyuqaq <1287062155@qq.com>
2021-12-24 14:51:15 +0800

    add files

---
+++ ybg-front/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import {deleteRequest, getRequest, postRequest, putRequest} from "./utils/api";
+
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+Vue.prototype.postRequest = postRequest
+Vue.prototype.putRequest = putRequest
+Vue.prototype.getRequest = getRequest
+Vue.prototype.deleteRequest = deleteRequest
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
---
+++ ybg-server/README.md
@@ -0,0 +1,5 @@
+**ybg(云办公应用)**
+
+前台 [vue](https://cn.vuejs.org/) , [element-ui](https://element.eleme.cn/#/zh-CN/component/quickstart), axios
+
+后台 springboot 权限认证[sa-token](https://sa-token.dev33.cn/doc/index.html#/) 
